e78d502a0b1dce3b53df0ea113d94421ac7816a5
Jose Albizures <jose@albizures.com>
2023-12-26 19:23:59 -0600

    feat: export types

--- src/v/index.ts
+++ src/v/index.ts
@@ -2,9 +2,12 @@ import { createProxy } from '../create'
 import { type HtmlChild, type HtmlConfig, createHtmlElement } from './html'
 import { type SvgChild, type SvgConfig, createSvgElement } from './svg'
 
-export { Fragment } from './fragment'
+export { Fragment, type FragmentElement } from './fragment'
 
 export const elements = {
 	html: createProxy<HtmlConfig, HtmlChild>(createHtmlElement),
 	svg: createProxy<SvgConfig, SvgChild>(createSvgElement),
 }
+
+export { type HtmlChild, type HtmlConfig, createHtmlElement, AnyHtmlElement } from './html'
+export { type SvgChild, type SvgConfig, createSvgElement, AnySvgElement } from './svg'
